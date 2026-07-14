2ea1bc48b9d5833f42ab4aa97477030a908851c2
anshul-busy <anshul@tech.busy.in>
2026-07-14 12:43:42 +0530

    Grade calculator

--- Day-06 Javascript/Grade calculator/srcipt.js
+++ Day-06 Javascript/Grade calculator/srcipt.js
@@ -3,21 +3,25 @@ function calculateGrade(){
 
     const marks = document.getElementById("marksInput").value
     const heading= document.getElementById("grade")
+     if(marks.value == ""){
+        alert("Value cannot be empty")
+        return false
+    }
     if(marks>=90){
-        console.log("A grade")
+        heading.innerText="A grade"
+
     }else if(marks<90 && marks>=75){
-        console.log("B grade")
+        heading.innerText="B grade"
+    
     }else if(marks<75 && marks>=60){
-        console.log("C grade")
+        heading.innerText="C grade"
     }else if(marks<60 && marks>=45){
-        console.log("D grade")
+        heading.innerText="D grade"
     }else {
-        console.log("E grade (FAIL)")
+        heading.innerText="E grade"
     }
 }
 
 function inputChecker(){
-    if(document.getElementById("marksInput").value = ""){
-        alert("Value cannot be empty")
-    }
+   
 }

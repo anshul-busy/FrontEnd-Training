0c9a45c641d8e698bd4001499390c3fec7dcbcce
anshul-busy <anshul@tech.busy.in>
2026-07-14 11:13:31 +0530

    Calculator logic part completed

--- Day-06 Javascript/script.js
+++ Day-06 Javascript/script.js
@@ -1,3 +1,14 @@
-function add(){
-    return 
+
+
+
+function  display(val){
+  document.getElementById("input") .value += val
 }
+
+function clearDisplay(){
+    document.getElementById("input").value = ""
+}
+
+function calculate(){
+   document.getElementById("input").value = eval(document.getElementById("input").value)
+   }

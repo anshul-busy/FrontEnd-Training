cb4fe8609482bad3c850aa1c25bebb35b3975156
anshul-busy <anshul@tech.busy.in>
2026-07-15 18:32:10 +0530

    mini project

---
+++ Mini_Web_Project/script.js
@@ -0,0 +1,42 @@
+const URL = "https://jsonplaceholder.typicode.com/users";
+
+async function fetchData() {
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error("Invalid URL");
+    }
+    const data = await response.json();
+    // console.log(data);
+    const container = document.getElementById("list");
+    container.innerHTML = "";
+    data.forEach((item) => {
+              
+      const element = document.createElement("div");
+      element.innerHTML = `<h3>${item.name}</h3> <p>@:-${item.username}</p> <p>Em:-${item.email}</p> <p>Ph:-${item.phone}</p>`;
+      container.appendChild(element);
+    });
+  } catch (error) {
+    console.log("error :- ", error);
+  }
+}
+fetchData();
+
+const searchInput = document.getElementById("search");
+const container = document.getElementById("list");
+
+searchInput.addEventListener("input", () => {
+  const filterValue = searchInput.value.toLowerCase();
+  
+  const items = container.querySelectorAll("div");
+
+  items.forEach(item => {
+    const text = item.textContent.toLowerCase();
+    
+    if (text.includes(filterValue)) {
+      item.style.display = ""; 
+    } else {
+      item.style.display = "none"; 
+    }
+  });
+});
